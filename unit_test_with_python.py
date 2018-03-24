fff671334dd1a56388dfea7f48de88d2d7015fd7
carlos puerto garcia <cpuertog@gmail.com>
2018-03-23 19:27:49 -0500

    integrando casos de prueba

--- unit_test_with_python.py
+++ unit_test_with_python.py
@@ -1,5 +1,6 @@
 import unittest
 import volesf
+import ejemplo
 
 #crear casos de prueba para las funciones
 # para eso debemos crear una clase
@@ -11,7 +12,12 @@ class Testvol_esf(unittest.TestCase):
         self.assertEqual(result,113.04)
 
         #113.04
-
+#Integra tus casos de prueba
+class Testejemplo_add(unittest.TestCase):
+    # Escribimos el metodo
+    def test_ejemplo_add(self):
+        result=ejemplo.add(7,3)
+        self.assertEqual(result,10)
 
 if __name__ == '__main__':
     unittest.main()
