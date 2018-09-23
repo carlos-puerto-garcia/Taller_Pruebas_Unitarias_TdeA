8c40397dcc0b8612f415dde21a95511b20e835c8
luis bedoya <lucho.bedoya1@hotmail.com>
2018-09-23 16:32:05 -0500

    Adicion de commit de test Circulos
    Se adiciona en el archivo unit_test la prueba unitaria en python del area de un circulo

--- unit_test_with_python.py
+++ unit_test_with_python.py
@@ -68,6 +68,12 @@ class testImpares(unittest.TestCase):
         self.assertGreater(result, 5)        
 
 
+class Test_Circulos(unittest.TestCase):
+    def test_area(self):
+        resultado = circulos.Radio_circulo(2)
+        self.assertEqual(resultado,12.566370614359172)
+
+
 if __name__ == '__main__':
     unittest.main()
 
